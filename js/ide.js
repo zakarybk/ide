0d1704813c29ba774c9fd842af6ec319b6b01aad
Zak <19314863+zakarybk@users.noreply.github.com>
2020-02-04 11:56:07 +0000

    Edits to make it better suited for my research prject. Includes limiting the language selection to c# only and testing how to 'inject' my own libraries and got stdin working

--- js/ide.js
+++ js/ide.js
@@ -22,7 +22,7 @@ var stderrEditor;
 var compileOutputEditor;
 var sandboxMessageEditor;
 
-var isEditorDirty = false;
+var isEditorDirty = true;
 var currentLanguageId;
 
 var $selectLanguage;
@@ -68,7 +68,7 @@ var layoutConfig = {
                     title: "STDIN",
                     isClosable: false,
                     componentState: {
-                        readOnly: false
+                        readOnly: true
                     }
                 }]
             }, {
@@ -366,8 +366,18 @@ function run() {
     stderrEditor.setValue("");
     compileOutputEditor.setValue("");
     sandboxMessageEditor.setValue("");
+	
+	// Inject our own class
+	var s = `public class Test {
+    public static void TestMe() {
+        System.Console.WriteLine("This test worked!");
+    }
+	}
+	`;
+	
+	var sourceValue = s + sourceEditor.getValue();
 
-    var sourceValue = encode(sourceEditor.getValue());
+    sourceValue = encode(sourceValue);
     var stdinValue = encode(stdinEditor.getValue());
     var languageId = resolveLanguageId($selectLanguage.val());
     var compilerOptions = $compilerOptions.val();
@@ -422,7 +432,7 @@ function fetchSubmission(submission_token) {
 
 function changeEditorLanguage() {
     monaco.editor.setModelLanguage(sourceEditor.getModel(), $selectLanguage.find(":selected").attr("mode"));
-    currentLanguageId = parseInt($selectLanguage.val());
+    currentLanguageId = 51 //csharp  //parseInt($selectLanguage.val());
     $(".lm_title")[0].innerText = fileNames[currentLanguageId];
     apiUrl = resolveApiUrl($selectLanguage.val());
     showApiUrl();
@@ -582,6 +592,8 @@ $(document).ready(function () {
             fontSize -= 1;
             editorsUpdateFontSize(fontSize);
         }
+		
+		console.log("Key pressed: " + keyCode);
     });
 
     $("select.dropdown").dropdown();
@@ -635,6 +647,11 @@ $(document).ready(function () {
                     enabled: false
                 }
             });
+			stdinEditor.setValue(`Test STDIN
+Test STDIN2
+Test STDIN3
+Test STDIN4
+Test STDIN5`);
         });
 
         layout.registerComponent("stdout", function (container, state) {
@@ -733,241 +750,32 @@ $(document).ready(function () {
 });
 
 // Template Sources
-var assemblySource = "\
-section	.text\n\
-    global _start\n\
-\n\
-_start:\n\
-\n\
-    xor	eax, eax\n\
-    lea	edx, [rax+len]\n\
-    mov	al, 1\n\
-    mov	esi, msg\n\
-    mov	edi, eax\n\
-    syscall\n\
-\n\
-    xor	edi, edi\n\
-    lea	eax, [rdi+60]\n\
-    syscall\n\
-\n\
-section	.rodata\n\
-\n\
-msg	db 'hello, world', 0xa\n\
-len	equ	$ - msg\n\
-";
-
-var bashSource = "echo \"hello, world\"";
-
-var basicSource = "PRINT \"hello, world\"";
-
-var cSource = "\
-#include <stdio.h>\n\
-\n\
-int main(void) {\n\
-    printf(\"hello, world\\n\");\n\
-    return 0;\n\
-}\n\
-";
 
 var csharpSource = "\
+// Dissertation test introduction \n\n\
 public class Hello {\n\
-    public static void Main() {\n\
-        System.Console.WriteLine(\"hello, world\");\n\
+    public static void Main(string[] args) {\n\
+		Test.TestMe();\n\
+\n\
+        string inputText = System.Console.ReadLine();\n\
+\n\
+        while (!string.IsNullOrEmpty(inputText))\n\
+        {\n\
+            System.Console.WriteLine(inputText);\n\
+            inputText = System.Console.ReadLine();\n\
+        }\n\
     }\n\
+    \n\
 }\n\
 ";
 
-var cppSource = "\
-#include <iostream>\n\
-\n\
-int main() {\n\
-    std::cout << \"hello, world\" << std::endl;\n\
-    return 0;\n\
-}\n\
-";
-
-var lispSource = "(write-line \"hello, world\")";
-
-var dSource = "\
-import std.stdio;\n\
-\n\
-void main()\n\
-{\n\
-    writeln(\"hello, world\");\n\
-}\n\
-";
-
-var elixirSource = "IO.puts \"hello, world\"";
-
-var erlangSource = "\
-main(_) ->\n\
-    io:fwrite(\"hello, world\\n\").\n\
-";
-
-var executableSource = "\
-Judge0 IDE assumes that content of executable is Base64 encoded.\n\
-\n\
-This means that you should Base64 encode content of your binary,\n\
-paste it here and click \"Run\".\n\
-\n\
-Here is an example of compiled \"hello, world\" NASM program.\n\
-Content of compiled binary is Base64 encoded and used as source code.\n\
-\n\
-https://ide.judge0.com/?kS_f\n\
-";
-
-var fortranSource = "\
-program main\n\
-    print *, \"hello, world\"\n\
-end\n\
-";
-
-var goSource = "\
-package main\n\
-\n\
-import \"fmt\"\n\
-\n\
-func main() {\n\
-    fmt.Println(\"hello, world\")\n\
-}\n\
-";
-
-var haskellSource = "main = putStrLn \"hello, world\"";
-
-var javaSource = "\
-public class Main {\n\
-    public static void main(String[] args) {\n\
-        System.out.println(\"hello, world\");\n\
-    }\n\
-}\n\
-";
-
-var javaScriptSource = "console.log(\"hello, world\");";
-
-var luaSource = "print(\"hello, world\")";
-
-var nimSource = "\
-# On the Judge0 IDE, Nim is automatically\n\
-# updated every day to the latest stable version.\n\
-echo \"hello, world\"\n\
-";
-
-var ocamlSource = "print_endline \"hello, world\"";
-
-var octaveSource = "printf(\"hello, world\\n\");";
-
-var pascalSource = "\
-program Hello;\n\
-begin\n\
-    writeln ('hello, world')\n\
-end.\n\
-";
-
-var phpSource = "\
-<?php\n\
-print(\"hello, world\\n\");\n\
-?>\n\
-";
-
-var plainTextSource = "hello, world\n";
-
-var prologSource = "\
-:- initialization(main).\n\
-main :- write('hello, world\\n').\n\
-";
-
-var pythonSource = "print(\"hello, world\")";
-
-var rubySource = "puts \"hello, world\"";
-
-var rustSource = "\
-fn main() {\n\
-    println!(\"hello, world\");\n\
-}\n\
-"
-
-var typescriptSource = "console.log(\"hello, world\");";
-
-var vSource = "\
-// On the Judge0 IDE, V is automatically\n\
-// updated every hour to the latest version.\n\
-fn main() {\n\
-    println('hello, world')\n\
-}\n\
-";
 
 var sources = {
-    45: assemblySource,
-    46: bashSource,
-    47: basicSource,
-    48: cSource,
-    49: cSource,
-    50: cSource,
     51: csharpSource,
-    52: cppSource,
-    53: cppSource,
-    54: cppSource,
-    55: lispSource,
-    56: dSource,
-    57: elixirSource,
-    58: erlangSource,
-    44: executableSource,
-    59: fortranSource,
-    60: goSource,
-    61: haskellSource,
-    62: javaSource,
-    63: javaScriptSource,
-    64: luaSource,
-    1000: nimSource,
-    65: ocamlSource,
-    66: octaveSource,
-    67: pascalSource,
-    68: phpSource,
-    43: plainTextSource,
-    69: prologSource,
-    70: pythonSource,
-    71: pythonSource,
-    72: rubySource,
-    73: rustSource,
-    74: typescriptSource,
-    1001: vSource
 };
 
 var fileNames = {
-    45: "main.asm",
-    46: "script.sh",
-    47: "main.bas",
-    48: "main.c",
-    49: "main.c",
-    50: "main.c",
     51: "Main.cs",
-    52: "main.cpp",
-    53: "main.cpp",
-    54: "main.cpp",
-    55: "script.lisp",
-    56: "main.d",
-    57: "script.exs",
-    58: "main.erl",
-    44: "a.out",
-    59: "main.f90",
-    60: "main.go",
-    61: "main.hs",
-    62: "Main.java",
-    63: "script.js",
-    64: "script.lua",
-    1000: "main.nim",
-    65: "main.ml",
-    66: "script.m",
-    67: "main.pas",
-    68: "script.php",
-    43: "text.txt",
-    69: "main.pro",
-    70: "script.py",
-    71: "script.py",
-    72: "script.rb",
-    73: "main.rs",
-    74: "script.ts",
-    1001: "main.v"
 };
 
 var languageIdTable = {
